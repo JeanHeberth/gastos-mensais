9f87acc5f851c04496cf6d40e0075d844a598049
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2025-11-06 17:52:30 -0300

    Refatorado os testes (#39)

--- src/test/java/br/com/gastosmensais/repository/GastoRepositoryTest.java
+++ src/test/java/br/com/gastosmensais/repository/GastoRepositoryTest.java
@@ -2,6 +2,7 @@ package br.com.gastosmensais.repository;
 
 import br.com.gastosmensais.config.AbstractIntegrationTest;
 import br.com.gastosmensais.entity.Gasto;
+import br.com.gastosmensais.util.TestDataFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,14 +20,7 @@ class GastoRepositoryTest extends AbstractIntegrationTest {
 
     @Test
     void deveSalvarEGastoNoMongo() {
-        Gasto gasto = new Gasto();
-        gasto.setDescricao("Aluguel");
-        gasto.setValorTotal(new BigDecimal("1200.00"));
-        gasto.setCategoria("Moradia");
-        gasto.setTipoPagamento("Pix");
-        gasto.setParcelas(1);
-        gasto.setDataCompra(LocalDateTime.now());
-
+        var gasto = TestDataFactory.criarGastoEntityPadrao();
         Gasto salvo = gastoRepository.save(gasto);
         assertThat(salvo.getId()).isNotNull();
     }
