b8dae027e00f5eef31de06df5fdca3ab004c8bc5
jeanheberth <jeanheberth19@gmail.com>
2025-11-06 16:37:06 -0300

    Corrigido teste

--- src/test/java/br/com/gastosmensais/util/TestDataFactory.java
+++ src/test/java/br/com/gastosmensais/util/TestDataFactory.java
@@ -3,6 +3,7 @@ package br.com.gastosmensais.util;
 
 
 import br.com.gastosmensais.dto.gasto.request.GastoRequestDTO;
+import br.com.gastosmensais.entity.Gasto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,6 +19,19 @@ public class TestDataFactory {
                 2,
                 LocalDateTime.now()
         );
+    }
+
+    // Entidade simulada de saída (mock do repositório)
+    public static Gasto criarGastoEntityPadrao() {
+        Gasto gasto = new Gasto();
+        gasto.setId("gasto-123");
+        gasto.setDescricao("Notebook");
+        gasto.setValorTotal(new BigDecimal("6000.00"));
+        gasto.setCategoria("Tecnologia");
+        gasto.setTipoPagamento("Cartão");
+        gasto.setParcelas(3);
+        gasto.setDataCompra(LocalDateTime.of(2025, 11, 6, 0, 0));
+        return gasto;
     }
 }
 
