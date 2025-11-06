924b29d6ce987b5e31d4c9beb92edd042b4fca67
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2025-11-06 12:46:03 -0300

    Criado entidade Gasto (#17) (#18)

---
+++ src/main/java/br/com/gastosmensais/entity/Gasto.java
@@ -0,0 +1,26 @@
+package br.com.gastosmensais.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "gastos")
+public class Gasto {
+
+    @Id
+    private String id;
+    private String descricao;
+    private BigDecimal valorTotal;
+    private String categoria;
+    private String tipoPagamento;
+    private Integer parcelas;
+    private LocalDateTime dataCompra;
+}
