b05b817bf619a3f8907a5cc647056692fcd37788
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2025-11-07 08:53:12 -0300

    Merge develop → main (#54)
    * alterado arquivos (#51)
    
    * Merge card6 → develop (#53)
    
    * alterado arquivos
    
    * adicionado arquivo para rodar no tomcat

--- src/main/java/br/com/gastosmensais/GastosMensaisApplication.java
+++ src/main/java/br/com/gastosmensais/GastosMensaisApplication.java
@@ -2,9 +2,16 @@ package br.com.gastosmensais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GastosMensaisApplication {
+public class GastosMensaisApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GastosMensaisApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GastosMensaisApplication.class, args);
