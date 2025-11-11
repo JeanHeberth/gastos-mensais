3e768ac8230842707e893375edbada296f58280e
jeanheberth <jeanheberth19@gmail.com>
2025-11-11 13:47:26 -0300

    Criado os passos do card 6

--- src/main/java/br/com/gastosmensais/controller/DashboardController.java
+++ src/main/java/br/com/gastosmensais/controller/DashboardController.java
@@ -4,11 +4,9 @@ import br.com.gastosmensais.dto.gasto.response.ResumoMensalResponseDTO;
 import br.com.gastosmensais.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/gastos/resumo")
 @RequiredArgsConstructor
